9f6cc9f8ad9219c8e87f48dd57890ffcf7558cf6
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-08-16 13:13:20 +0300

    initial

---
+++ PokemonApp/Models/PokeApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIHelpers;
+
+namespace PokemonApp.Models
+{
+    public class PokeApi
+    {
+        const string url = "https://api.pokemontcg.io/v2/";
+
+        public static async Task<PokemonCard> GetSingleCard(string cardId)
+        {
+            string urlParams = "cards/" + cardId;
+
+            PokemonCard response = await ApiHelper.RunAsync<PokemonCard>(url, urlParams);
+
+            return response;
+        }
+    }
+}
