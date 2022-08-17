c9d083b4ad936cea9e8ed7c73ef284441ac80512
Jepsuli <hanninen.jerejohannes@gmail.com>
2022-08-17 09:44:38 +0300

    Kortti movement kuva APIsta

---
+++ PokemonApp/wwwroot/js/korttiliike.js
@@ -0,0 +1,44 @@
+﻿
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.kortti');
+
+// Täyttöä 
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Tyhjien boxien 
+for (const kortti of empties) {
+    kortti.addEventListener('dragover', dragOver);
+    kortti.addEventListener('dragenter', dragEnter);
+    kortti.addEventListener('dragleave', dragLeave);
+    kortti.addEventListener('drop', dragDrop);
+}
+
+// Raahaus funktiot
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'kortti';
+}
+
+function dragDrop() {
+    this.className = 'kortti';
+    this.append(fill);
+}
