d4d0b3ad9ff965e3c832ff98e367f3da1042d214
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-08-22 17:34:04 +0300

    read json file

--- PokemonApp/Models/CardMethods.cs
+++ PokemonApp/Models/CardMethods.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
-using PokemonTcgSdk;
-using PokemonTcgSdk.Models;
+using System.Text.Json;
 
 namespace PokemonApp.Models
 {
     public class CardMethods
     {
-        
+        public static PokemonCards GetPokemonCards()
+        {
+            string json = File.ReadAllText("./wwwroot/js/pokemons.json");
+            var pokemonCards = JsonSerializer.Deserialize<PokemonCards>(json);
 
-
+            return pokemonCards;
+        }
     }
 }
