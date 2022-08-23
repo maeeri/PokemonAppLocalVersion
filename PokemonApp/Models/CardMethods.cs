4679325a6481dfe395788e87a5f93aa4bd7d5331
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-08-23 15:35:11 +0300

    tried

--- PokemonApp/Models/CardMethods.cs
+++ PokemonApp/Models/CardMethods.cs
@@ -20,7 +20,8 @@ namespace PokemonApp.Models
             return separateCards;
         }
 
-        //find certain types of cards (Energy/Pokémon/Trainer)
+        //find certain types of cards (Energy/Pokémon/Trainer), doesn't work yet,because limited number of cards
+        //in json file
         public static List<Datum> FindCards(string supertype)
         {
             var allCards = GetPokemonCards();
@@ -31,12 +32,15 @@ namespace PokemonApp.Models
         //find cards by rarity(Amazing Rare/Classic Collection/Common/LEGEND/Promo/Radiant Rare/Rare/Rare ACE/
         //Rare BREAK/Rare Holo/Rare Holo EX/Rare Holo GX/Rare Holo LV.X/Rare Holo Star/Rare Holo V/Rare Holo VMAX/,
         //Rare Holo VSTAR/Rare Prime/Rare Prism Star/Rare Rainbow/Rare Secret/Rare Shining/Rare Shiny/Rare Shiny GX/Rare Ultra/Uncommon/V/VM)
-        public static List<Datum> GetCardsByRarity(string rarity)
-        {
-            var valivaihe = GetPokemonCards().Where(x => x.rarity.Contains("common", StringComparison.OrdinalIgnoreCase));
-            var theseCards = valivaihe.ToList();
-            return theseCards;
-        }
+        //public static List<Datum> GetCardsByRarity(string rarity)
+        //{
+        //    var allCards = GetPokemonCards();
+        //    var theseCardsIe = from card in allCards
+        //        where card.rarity.Contains(rarity, StringComparison.OrdinalIgnoreCase)
+        //        select card;
+        //    var theseCards = theseCardsIe.ToList();
+        //    return theseCards;
+        //}
 
         //search cards by name
         public static List<Datum> GetCardsByName(string searchString)
