63f1068aa8221f551c9ca391018416117e627e50
lapinro <roosa.lapintaival@kotinet.com>
2022-08-24 14:07:23 +0300

    pull

--- PokemonApp/Program.cs
+++ PokemonApp/Program.cs
@@ -19,15 +19,10 @@ namespace PokemonApp
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-})
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    Host.CreateDefaultBuilder(args)
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                        });
     }
 }
